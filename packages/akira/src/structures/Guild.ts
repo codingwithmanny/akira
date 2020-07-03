ffff9c09394ccf2c2d40f652d17e50d5845bff5b
pandaniell <daniell.7@icloud.com>
2020-07-03 15:14:45 +0200

    Create set method for guild settings manager

--- packages/akira/src/structures/Guild.ts
+++ packages/akira/src/structures/Guild.ts
@@ -1,5 +1,8 @@
 import { Client, Structures } from "discord.js";
+import { getRepository } from "typeorm";
+import { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity";
 import { GuildSettings, IGuildSettings } from "../entity/GuildSettings";
+import { MaybeArray } from "../util/utilities";
 
 export = Structures.extend("Guild", (Guild) => {
   return class extends Guild {
@@ -19,6 +22,29 @@ export = Structures.extend("Guild", (Guild) => {
       }
 
       return guildSettings;
+    }
+
+    async set(values: MaybeArray<QueryDeepPartialEntity<GuildSettings>>) {
+      const keys = Object.keys(Array.isArray(values) ? values[0] : values);
+
+      if (!keys.length) {
+        throw new Error("Cannot upsert without any values specified");
+      }
+
+      values = Array.isArray(values)
+        ? values.map((value) => ({ ...value, guildId: this.id }))
+        : { ...values, guildId: this.id };
+
+      const updateStr = keys
+        .map((key) => `"${key}" = EXCLUDED."${key}"`)
+        .join(", ");
+
+      return getRepository(GuildSettings)
+        .createQueryBuilder()
+        .insert()
+        .values(values)
+        .onConflict(`("guildId") DO UPDATE SET ${updateStr}`)
+        .execute();
     }
   };
 });
--- packages/akira/types/index.d.ts
+++ packages/akira/types/index.d.ts
@@ -1,9 +1,15 @@
-import { IGuildSettings } from "../src/entity/GuildSettings";
+import { InsertResult } from "typeorm";
+import { QueryDeepPartialEntity } from "typeorm/query-builder/QueryPartialEntity";
+import { GuildSettings, IGuildSettings } from "../src/entity/GuildSettings";
+import { MaybeArray } from "../src/util/utilities";
 
 declare module "discord.js" {
   interface Guild {
     get<T extends (keyof IGuildSettings)[]>(
       keys: T
     ): Promise<Pick<IGuildSettings, T extends (infer U)[] ? U : never>>;
+    set(
+      values: MaybeArray<QueryDeepPartialEntity<GuildSettings>>
+    ): Promise<InsertResult>;
   }
 }
