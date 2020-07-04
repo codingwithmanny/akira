2f01fc3674e78674c910711b5ed3dd9b40a66c98
pandaniell <daniell.7@icloud.com>
2020-07-04 13:26:11 +0200

    Remove comments that have no effect

--- packages/akira/src/entity/GuildSettings.ts
+++ packages/akira/src/entity/GuildSettings.ts
@@ -18,17 +18,10 @@ export class GuildSettings extends BaseEntity implements IGuildSettings {
   @PrimaryColumn()
   guildId!: string;
 
-  @Column({
-    comment: "The prefix of the guild to trigger the bot (e.g., !, a!, $)",
-    default: process.env.PREFIX,
-  })
+  @Column({ default: process.env.PREFIX })
   prefix!: string;
 
-  @Column({
-    comment:
-      "The id of the user who was the last one to update the guild settings",
-    default: process.env.CLIENT_ID,
-  })
+  @Column({ default: process.env.CLIENT_ID })
   updatedBy!: string;
 
   @UpdateDateColumn()
