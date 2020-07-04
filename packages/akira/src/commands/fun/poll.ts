48912367907e070931b660f004154be211f37991
pandaniell <daniell.7@icloud.com>
2020-07-04 20:29:14 +0200

    Create poll command

---
+++ packages/akira/src/commands/fun/poll.ts
@@ -0,0 +1,88 @@
+import { Message, MessageEmbedOptions } from "discord.js";
+import { Question } from "../../entity/Question";
+import { Command } from "../../util/registerCommandsAndEvents";
+import { numericEmojis } from "../../util/utilities";
+
+type Reply = typeof replies[number];
+
+const replies = ["y", "n", undefined] as const;
+
+const filter = (message: Message) => {
+  return replies.includes(message.content.toLowerCase() as Reply);
+};
+
+export const command: Command<string[]> = {
+  description: "Start a public or anonymous poll",
+  usage: "<question, answers separated by |>",
+  examples: ["What is the best looking color? | Red | Green | Blue"],
+  acceptsArgs: true,
+  requiresArgs: true,
+  clientPermissions: ["EMBED_LINKS", "READ_MESSAGE_HISTORY", "ADD_REACTIONS"],
+  userPermissions: ["MANAGE_GUILD"],
+  validateArgs: (_, args) => args.join(" ").split(" | "),
+  async execute(message, [question, ...answers]) {
+    if (answers.length < 2 || answers.length > 10) {
+      return message.reply(
+        "please provide at least 2 possible answers with a maximum of 10"
+      );
+    }
+
+    const prompt = await message.channel.send(
+      "Do you want this poll to be anonymous? (Y/N)"
+    );
+
+    const reply = (await message.channel
+      .awaitMessages(filter, { max: 1, time: 60000 })
+      .then((response) => response.first()?.content)) as Reply;
+
+    await prompt.delete();
+
+    if (!reply) {
+      return message.reply(
+        "poll command will be cancelled because you waited too long"
+      );
+    }
+
+    const embed: MessageEmbedOptions = {
+      color: "BLUE",
+      author: {
+        name: message.author.tag,
+        iconURL: message.author.displayAvatarURL(),
+      },
+      title: question,
+      description: answers
+        .map((answer, idx) => `${[numericEmojis][idx]}: ${answer}`)
+        .join("\n"),
+      footer: {
+        text: "The creator of this poll can end this poll by clicking ✅",
+      },
+    };
+
+    const pollMessage = await message.channel.send({ embed });
+
+    for (const [idx] of answers.entries()) {
+      await pollMessage.react(numericEmojis[idx]);
+    }
+
+    await pollMessage.react("✅");
+
+    if (message.deletable) {
+      await message.delete();
+    }
+
+    if (reply === "n") {
+      return;
+    }
+
+    embed.description = `${embed.description}\n\n*This poll is anonymous, your answer will be hidden but may be changed at any time*`;
+
+    await pollMessage.edit({ embed });
+
+    return Question.insert({
+      messageId: pollMessage.id,
+      authorId: message.author.id,
+      question,
+      possibleAnswers: answers,
+    });
+  },
+};
--- packages/akira/src/util/utilities.ts
+++ packages/akira/src/util/utilities.ts
@@ -1,1 +1,14 @@
 export type MaybeArray<T> = T | T[];
+
+export const numericEmojis = [
+  "1⃣",
+  "2⃣",
+  "3⃣",
+  "4⃣",
+  "5⃣",
+  "6⃣",
+  "7⃣",
+  "8⃣",
+  "9⃣",
+  "🔟",
+];
